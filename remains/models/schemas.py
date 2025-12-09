68667abd67e81376a17da5673a4e6302accdb21b
AnjanThakur <thakuranjan2006@gmail.com>
2025-12-09 15:45:18 +0530

    Remove remains folder from tracking

--- remains/models/schemas.py
+++
@@ -1,9 +0,0 @@
-# models/schemas.py
-from pydantic import BaseModel
-from typing import Optional
-
-
-class OCRReq(BaseModel):
-    imageBase64: str
-    template: Optional[str] = None
-    debug: Optional[bool] = False
